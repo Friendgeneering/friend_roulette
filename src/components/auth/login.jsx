b5a4496b84b67fa45efd3bdb45e04eec07c4c48a
Lee Mordell <lee.mordell@gmail.com>
2016-12-26 15:25:57 -0800

    [pull] basic form layout complete

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,16 +1,43 @@
 import React, { Component } from 'react'
 
 import { base } from '../base'
+import { monthsAndDays, getNumOfDays, birthYears } from './util'
 
 
 export default class Login extends Component {
 	constructor(props) {
 		super(props)
 
-		this.state = {}
+		this.state = {
+			birthday: {
+				month: 'January',
+				day: 1,
+				year: 1995
+			}
+		}
+
+		this.renderDays = this.renderDays.bind(this)
+		this.handleDateChange = this.handleDateChange.bind(this)
+	}
+
+	handleDateChange(e, type) {
+		// Generalized function to handle month, day, and year state changes
+		let newState = this.state
+		newState.birthday[type] = e.target.value
+		this.setState({...this.state, ...newState})
+	}
+
+	renderDays() {
+		const { month } = this.state.birthday
+		const numOfdays = getNumOfDays(monthsAndDays[month])
+		return numOfdays.map(day => <option key={day} value={day}>{day}</option>)
 	}
 
 	render() {
+
+		const { month, day, year } = this.state.birthday
+		let allBirthYears = birthYears()
+
 		return (
 			<div>
 			<base.navbar />
@@ -20,11 +47,70 @@ export default class Login extends Component {
 				    <li role="presentation"><a href="#signup" aria-controls="signup" role="tab" data-toggle="tab">Sign Up</a></li>
 	  			</ul>
 
-
-
 				<div className="tab-content">
-				    <div role="tabpanel" className="tab-pane active" id="login">Login</div>
-				    <div role="tabpanel" className="tab-pane" id="signup">Sign Up</div>
+			{/*Login Panel*/}
+				    <div role="tabpanel" className="tab-pane active" id="login">
+				    	<div className="input-group">
+  							<span className="input-group-addon"><span className="glyphicon glyphicon-user"></span></span>
+ 							<input type="text" className="form-control" placeholder="Username" />
+						</div>
+						<div className="input-group">
+  							<span className="input-group-addon"><span className="glyphicon glyphicon-option-horizontal"></span></span>
+ 							<input type="password" className="form-control" placeholder="Password" />
+						</div>
+						<button type="button" className="btn btn-primary">Submit</button>
+				    </div>
+
+			{/*Signup Panel*/}
+				    <div role="tabpanel" className="tab-pane" id="signup">
+				    	<div className="input-group">
+  							<span className="input-group-addon"><span className="glyphicon glyphicon-user"></span></span>
+ 							<input type="text" className="form-control" placeholder="Username" />
+						</div>
+						<div className="input-group">
+  							<span className="input-group-addon"><span className="glyphicon glyphicon-option-horizontal"></span></span>
+ 							<input type="password" className="form-control" placeholder="Password" />
+						</div>
+						<div className="input-group">
+  							<span className="input-group-addon"><span className="glyphicon glyphicon-option-horizontal"></span></span>
+ 							<input type="password" className="form-control" placeholder="Confirm Password" />
+						</div>
+						<div className="input-group">
+  							<span className="input-group-addon">Birthday </span>
+  							<div className="col-xs-4">
+  							<select className="form-control" name="month" value={month} onChange={(e) => this.handleDateChange(e, 'month')}>
+  								{Object.keys(monthsAndDays).map(month => <option key={month} value={month}>{month}</option>)}
+							</select>
+							</div>
+							<div className="col-xs-3">
+							<select  className="form-control" name="day" value={day} onChange={(e) => this.handleDateChange(e, 'day')}>
+  								{this.renderDays()}
+							</select>
+							</div>
+							<div className="col-xs-3">
+							<select  className="form-control" name="year" value={year} onChange={(e) => this.handleDateChange(e, 'year')}>
+  								{allBirthYears.map(year => <option key={year} value={year}>{year}</option>)}
+							</select>
+							</div>
+						</div>
+						<div className="input-group">
+							<span className="input-group-addon">Gender</span>
+  							<label className="radio-inline">
+						      <input type="radio" name="optradio" />Male
+						    </label>
+						    <label className="radio-inline">
+						      <input type="radio" name="optradio" />Female
+						    </label>
+						    <label className="radio-inline">
+						      <input type="radio" name="optradio" />Other
+						    </label>
+						</div>
+						<div className="input-group">
+  							<span className="input-group-addon"><span className="glyphicon glyphicon-map-marker"></span></span>
+ 							<input type="password" className="form-control" placeholder="Location" />
+						</div>
+						<button type="submit" className="btn btn-primary">Submit</button>
+				    </div>
 				</div>
 			</div>
 			</div>
---
+++ src/components/auth/util.js
@@ -0,0 +1,34 @@
+//Helper object containing month and corresponding # of days
+
+export const monthsAndDays = {
+"January": 31,
+"February": 29,
+"March": 31,
+"April": 30,
+"May": 31,
+"June": 30,
+"July": 31,
+"August": 31,
+"September": 30,
+"October": 31,
+"November": 30,
+"December": 31
+}
+
+//Function to change # of days depending on month
+export const getNumOfDays = (num) => {
+	let result = []
+	for(let i = 1; i <= num; i++) {
+		result.push(i)
+	}
+	return result
+}
+
+//Function to change # of days depending on month
+export const birthYears = () => {
+	let result = []
+	for(let i = 1925; i <= 2013; i++) {
+		result.push(i)
+	}
+	return result
+}
