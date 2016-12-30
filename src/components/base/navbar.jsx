0d4806cc8f43b783e1031f61d5737933b1622d1a
Lee Mordell <lee.mordell@gmail.com>
2016-12-29 17:57:43 -0800

    [pull] updated navbar styling

--- src/components/base/navbar.jsx
+++ src/components/base/navbar.jsx
@@ -12,9 +12,19 @@ export default class Navbar extends Component {
 		return (
 			<div>
 			<nav className="navbar navbar-default">
-  				<div className="container">
-    				<ul className="nav navbar-nav">
-    					<li><a href="#">Link</a></li>
+  				<div className="nav-container">
+  					<a className="navbar-brand">Friend Roulette</a>
+    				<ul className="nav navbar-right">
+    				<div className="dropdown">
+    				<button id="nav-profile" className="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
+					    <i className="material-icons">account_circle</i>
+					    <span className="caret"></span>
+					    </button>
+					    <ul className="dropdown-menu" role="menu" aria-labelledby="menu1">
+					      <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">Profile</a></li>
+					      <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">Logout</a></li>
+					    </ul>
+  					</div>
     				</ul>
   				</div>
 			</nav>
